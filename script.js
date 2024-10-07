e59666bc939ae62e2077e658caf30d9520a72faa
GabrielNScepanski <161836677+GabrielNScepanski@users.noreply.github.com>
2024-10-07 09:42:04 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+function somar (a,b){
+ let total= a=b
+ console = a+b
+ console.log(total)
+}
+somar(2,10)
+
+let subtrair =(num1,num2)=>{
+    let tital= num1-num2
+    console.log(total)
+}
+
+subtrair(15,17)
+
+let numeros =[1,2,3,10]
+numeros.map((item)=>{
+    console.log(item)
+})
